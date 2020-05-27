a135277cc726f77685d45b179ab269463298df8f
THORBEN ANDRESEN <thorben.andresen@gmail.com>
2020-05-27 15:12:45 -0400

    refactors implementation, adds documentation markup

--- Sources/SM2Lib/SM2Lib.swift
+++ Sources/SM2Lib/SM2Lib.swift
@@ -8,15 +8,16 @@
 import Foundation
 
 public enum SM2Error: Error, LocalizedError {
-    case gradeOutOfRange(Int)
-
+    /// Grade is not valid valid
+    case invalidGrade(Int)
+    
     public var errorDescription: String? {
         switch self {
-        case .gradeOutOfRange(let grade):
-            return "Grade \(grade) is out of range"
+        case .invalidGrade(let grade):
+            return "Grade \(grade) is not valid"
         }
     }
-
+    
 }
 
 /**  Grades from SuperMemo Algorithm
@@ -40,8 +41,8 @@ public enum SM2Grade: Int, CustomStringConvertible, CaseIterable {
     case good
     /// perfect response
     case bright
-
-   public var description: String {
+    
+    public var description: String {
         switch self {
         case .null:
             return NSLocalizedString("complete blackout", comment: "")
@@ -55,12 +56,13 @@ public enum SM2Grade: Int, CustomStringConvertible, CaseIterable {
             return NSLocalizedString("correct response after a hesitation", comment: "")
         case .bright:
             return NSLocalizedString("perfect response", comment: "")
-
+            
         }
     }
 }
 
-public protocol SM2GradeDataType {
+/// A type which describes the properties required to calculate the SM2 parameter
+public protocol SM2CardParameterType {
     var repetition: Int { get }
     var interval: Int { get }
     var easinessFactor: Double { get }
@@ -68,13 +70,21 @@ public protocol SM2GradeDataType {
     var nextDate: TimeInterval { get }
 }
 
-public struct SM2GradeData: SM2GradeDataType {
+/// Struct holding the SM2 parameter for a card
+public struct SM2CardParameter: SM2CardParameterType {
     public var repetition: Int
     public var interval: Int
     public var easinessFactor: Double
     public var previousDate: TimeInterval
     public var nextDate: TimeInterval
-
+    
+    /// Description
+    /// - Parameters:
+    ///   - repetition: card repetitions
+    ///   - interval: interval in days until next due date
+    ///   - easinessFactor: easiness factor reflecting the easiness of memorizing and retaining a given item in memory
+    ///   - previousDate: previous due date
+    ///   - nextDate: next due date
     public init(
         repetition: Int = 0,
         interval: Int = 0,
@@ -82,99 +92,117 @@ public struct SM2GradeData: SM2GradeDataType {
         previousDate: TimeInterval? = nil,
         nextDate: TimeInterval? = nil
     ) {
-
+        
         let currentDate = Date().timeIntervalSince1970
-
+        
         self.repetition = repetition
         self.interval = interval
         self.easinessFactor = easinessFactor
-
+        
         if
             let previousDate = previousDate,
             let nextDate = nextDate {
-
+            
             self.previousDate = previousDate
             self.nextDate = nextDate
-
+            
         } else {
             self.previousDate = currentDate
-               self.nextDate = currentDate
+            self.nextDate = currentDate
         }
-
+        
     }
 }
 
+/// Namespaceing the SM2Engine bfor better readability
 public struct SM2Engine {
-
-    /// Max Grade `Grade.bright`
-    /// The default value is 5
-    public static func gradeFlashCard(gradeData: SM2GradeDataType, grade: SM2Grade, currentDatetime: TimeInterval, maxGrade: Int = 5) -> SM2GradeDataType {
-
-        let cardGrade = grade.rawValue
-
-        // The default value is 1.3
-        var easinessFactor = 1.3
-        var repetition: Int
-        var interval: Int
-        var previousDate: TimeInterval?
-        var nextDate: TimeInterval
-
-        if cardGrade < 3 {
-            repetition = 0
-            interval = 0
-            easinessFactor = gradeData.easinessFactor
-        } else {
-
-            // Easiness Factor
-            let qualityFactor = Double(maxGrade - cardGrade) // CardGrade.bright.rawValue - grade
-
-            let newEasinessFactor = gradeData.easinessFactor + (0.1 - qualityFactor * (0.08 + qualityFactor * 0.02))
-
-            if newEasinessFactor < easinessFactor {
-               // easinessFactor = gradeData.easinessFactor
-            } else {
-                easinessFactor = newEasinessFactor
-            }
-
-            // Repetition
-            repetition = gradeData.repetition + 1
-
-            // Interval
-            switch repetition {
-            case 1:
-                interval = 1
-            case 2:
-                interval = 6
-            default:
-                let newInterval = ceil(Double(repetition - 1) * easinessFactor)
-                interval = Int(newInterval)
-            }
-        }
-
-        if cardGrade == 3 {
-            interval = 0
-        }
-
-        let seconds = 60
-        let minutes = 60
-        let hours = 24
-        let dayMultiplier = seconds * minutes * hours // 1
-        let extraDays = dayMultiplier * interval
-        let newNextDatetime = currentDatetime + Double(extraDays)
-
-        previousDate = gradeData.nextDate
-        nextDate = newNextDatetime
-
-        let gradeData = SM2GradeData(
+    
+    /// Updates SM2CardParameter based on SM2Grade
+    /// - Parameters:
+    ///   - cardParameter: SM2CardParameter
+    ///   - grade: Grade
+    ///   - currentDatetime: current time
+    ///   - maxGrade: max grade possible
+    /// - Returns: a new SM2CardParameter instance with the updated parameter
+    public static func gradeFlashCard(cardParameter: SM2CardParameterType, grade: SM2Grade, currentDatetime: TimeInterval, maxGrade: Int = 5) -> SM2CardParameterType {
+        
+        let rawGrade = grade.rawValue
+        
+        let easinessFactor = calculateEasinessFactor(grade: rawGrade, maxGrade: maxGrade, previousEasinessFactor: cardParameter.easinessFactor)
+        let repetition = calculateRepetition(grade: rawGrade, previousRepetition: cardParameter.repetition)
+        let interval = calculateIntervalInDays(grade: rawGrade, repetition: repetition, easinessFactor: easinessFactor)
+        let previousDate = cardParameter.nextDate
+        let nextDate = addTimeIntervalToDate(intervalInDays: interval, timeInterval: currentDatetime)
+  
+        let gradeData = SM2CardParameter(
             repetition: repetition,
             interval: interval,
             easinessFactor: easinessFactor,
             previousDate: previousDate,
             nextDate: nextDate
         )
-
+        
         return gradeData
+    }
+    
+    
+    private static func calculateRepetition(grade: Int, previousRepetition: Int) -> Int {
+        
+        if grade < 3 {
+            return 0
+        }
+        
+        return previousRepetition + 1
+    }
+ 
+    
+    private static func calculateEasinessFactor(grade: Int, maxGrade: Int, previousEasinessFactor: Double) -> Double {
+        
+        if grade < 3 {
+           return previousEasinessFactor
+        }
+        
+        var easinessFactor = 1.3
+        let qualityFactor = Double(maxGrade - grade)
+        let newEasinessFactor = previousEasinessFactor + (0.1 - qualityFactor * (0.08 + qualityFactor * 0.02))
+        
+        if newEasinessFactor > easinessFactor {
+            easinessFactor = newEasinessFactor
+        }
+        
+        return easinessFactor
+    }
+    
+    private static func calculateIntervalInDays(grade: Int ,repetition: Int, easinessFactor: Double) -> Int {
+        
+        if grade <= 3 {
+            return 0
+        }
+        
+        var interval: Int
+        switch repetition {
+        case 1:
+            interval = 1
+        case 2:
+            interval = 6
+        default:
+            let newInterval = ceil(Double(repetition - 1) * easinessFactor)
+            interval = Int(newInterval)
+        }
+        return interval
+        
+    }
+    /// Adds a time interval in days to a date
+    /// Time intervals are used instead of calender calculations because the algorithm does not care about dates.
+    private static func addTimeIntervalToDate(intervalInDays: Int, timeInterval: TimeInterval) -> TimeInterval {
+        let seconds = 60
+        let minutes = 60
+        let hours = 24
+        let dayMultiplier = seconds * minutes * hours // 1
+        let extraDays = dayMultiplier * intervalInDays
+        return timeInterval + Double(extraDays)
+        
     }
 }
 
-//  1 - Time intervals are used instead of calender calculations because the algorithm does not care about dates.
+
--- Tests/SM2LibTests/SM2LibTests.swift
+++ Tests/SM2LibTests/SM2LibTests.swift
@@ -7,7 +7,7 @@ final class SM2LibTests: XCTestCase {
     static let defaultInterval = 0
     static let defaultEasinessFactor = 2.5
     static let october_23_2016 = 1477207892.0
-    static let defaultGradeData = SM2GradeData(repetition: defaultRepetion, interval: defaultInterval, easinessFactor: defaultEasinessFactor, previousDate: october_23_2016, nextDate: october_23_2016)
+    static let defaultCardParameter = SM2CardParameter(repetition: defaultRepetion, interval: defaultInterval, easinessFactor: defaultEasinessFactor, previousDate: october_23_2016, nextDate: october_23_2016)
 
     func testGradeDescription() {
         XCTAssertEqual(SM2Grade.null.description, "complete blackout")
@@ -18,9 +18,9 @@ final class SM2LibTests: XCTestCase {
         XCTAssertEqual(SM2Grade.bright.description, "perfect response")
     }
 
-    func testGradeData() {
+    func testCardParameter() {
 
-        let actual = SM2LibTests.defaultGradeData
+        let actual = SM2LibTests.defaultCardParameter
         XCTAssertEqual(actual.repetition, SM2LibTests.defaultRepetion)
         XCTAssertEqual(actual.interval, SM2LibTests.defaultInterval)
         XCTAssertEqual(actual.easinessFactor, SM2LibTests.defaultEasinessFactor)
@@ -30,53 +30,53 @@ final class SM2LibTests: XCTestCase {
     func testgrade_0_response() {
         let sm2grade = SM2Grade.null
         let expectedEasinessFactor = SM2LibTests.defaultEasinessFactor
-        let gradeData = SM2LibTests.defaultGradeData
-        let newGradeData = SM2Engine.gradeFlashCard(gradeData: gradeData, grade: sm2grade, currentDatetime: SM2LibTests.october_23_2016)
+        let cardParameter = SM2LibTests.defaultCardParameter
+        let newCardParameter = SM2Engine.gradeFlashCard(cardParameter: cardParameter, grade: sm2grade, currentDatetime: SM2LibTests.october_23_2016)
 
-        XCTAssertEqual(newGradeData.repetition, SM2LibTests.defaultRepetion)
-        XCTAssertEqual(newGradeData.interval, SM2LibTests.defaultInterval)
-        XCTAssertEqual(newGradeData.easinessFactor, expectedEasinessFactor)
-        XCTAssertEqual(newGradeData.previousDate, SM2LibTests.october_23_2016)
-        XCTAssertEqual(newGradeData.nextDate, SM2LibTests.october_23_2016)
+        XCTAssertEqual(newCardParameter.repetition, SM2LibTests.defaultRepetion)
+        XCTAssertEqual(newCardParameter.interval, SM2LibTests.defaultInterval)
+        XCTAssertEqual(newCardParameter.easinessFactor, expectedEasinessFactor)
+        XCTAssertEqual(newCardParameter.previousDate, SM2LibTests.october_23_2016)
+        XCTAssertEqual(newCardParameter.nextDate, SM2LibTests.october_23_2016)
     }
 
     func testgrade_1_response() {
         let sm2grade = SM2Grade.bad
         let expectedEasinessFactor = SM2LibTests.defaultEasinessFactor
-        let gradeData = SM2LibTests.defaultGradeData
-        let newGradeData = SM2Engine.gradeFlashCard(gradeData: gradeData, grade: sm2grade, currentDatetime: SM2LibTests.october_23_2016)
+        let cardParameter = SM2LibTests.defaultCardParameter
+        let newCardParameter = SM2Engine.gradeFlashCard(cardParameter: cardParameter, grade: sm2grade, currentDatetime: SM2LibTests.october_23_2016)
 
-        XCTAssertEqual(newGradeData.repetition, SM2LibTests.defaultRepetion)
-        XCTAssertEqual(newGradeData.interval, SM2LibTests.defaultInterval)
-        XCTAssertEqual(newGradeData.easinessFactor, expectedEasinessFactor)
-        XCTAssertEqual(newGradeData.previousDate, SM2LibTests.october_23_2016)
-        XCTAssertEqual(newGradeData.nextDate, SM2LibTests.october_23_2016)
+        XCTAssertEqual(newCardParameter.repetition, SM2LibTests.defaultRepetion)
+        XCTAssertEqual(newCardParameter.interval, SM2LibTests.defaultInterval)
+        XCTAssertEqual(newCardParameter.easinessFactor, expectedEasinessFactor)
+        XCTAssertEqual(newCardParameter.previousDate, SM2LibTests.october_23_2016)
+        XCTAssertEqual(newCardParameter.nextDate, SM2LibTests.october_23_2016)
     }
 
     func testgrade_2_response() {
         let sm2grade = SM2Grade.bad
         let expectedEasinessFactor = SM2LibTests.defaultEasinessFactor
-        let gradeData = SM2LibTests.defaultGradeData
-        let newGradeData = SM2Engine.gradeFlashCard(gradeData: gradeData, grade: sm2grade, currentDatetime: SM2LibTests.october_23_2016)
+        let cardParameter = SM2LibTests.defaultCardParameter
+        let newCardParameter = SM2Engine.gradeFlashCard(cardParameter: cardParameter, grade: sm2grade, currentDatetime: SM2LibTests.october_23_2016)
 
-        XCTAssertEqual(newGradeData.repetition, SM2LibTests.defaultRepetion)
-        XCTAssertEqual(newGradeData.interval, SM2LibTests.defaultInterval)
-        XCTAssertEqual(newGradeData.easinessFactor, expectedEasinessFactor)
-        XCTAssertEqual(newGradeData.previousDate, SM2LibTests.october_23_2016)
-        XCTAssertEqual(newGradeData.nextDate, SM2LibTests.october_23_2016)
+        XCTAssertEqual(newCardParameter.repetition, SM2LibTests.defaultRepetion)
+        XCTAssertEqual(newCardParameter.interval, SM2LibTests.defaultInterval)
+        XCTAssertEqual(newCardParameter.easinessFactor, expectedEasinessFactor)
+        XCTAssertEqual(newCardParameter.previousDate, SM2LibTests.october_23_2016)
+        XCTAssertEqual(newCardParameter.nextDate, SM2LibTests.october_23_2016)
     }
 
     func testgrade_3_response() {
         let sm2grade = SM2Grade.pass
         let expectedEasinessFactor = 2.36
-        let gradeData = SM2LibTests.defaultGradeData
-        let newGradeData = SM2Engine.gradeFlashCard(gradeData: gradeData, grade: sm2grade, currentDatetime: SM2LibTests.october_23_2016)
+        let cardParameter = SM2LibTests.defaultCardParameter
+        let newCardParameter = SM2Engine.gradeFlashCard(cardParameter: cardParameter, grade: sm2grade, currentDatetime: SM2LibTests.october_23_2016)
 
-        XCTAssertEqual(newGradeData.repetition, 1)
-        XCTAssertEqual(newGradeData.interval, SM2LibTests.defaultInterval)
-        XCTAssertEqual(newGradeData.easinessFactor, expectedEasinessFactor)
-        XCTAssertEqual(newGradeData.previousDate, SM2LibTests.october_23_2016)
-        XCTAssertEqual(newGradeData.nextDate, SM2LibTests.october_23_2016)
+        XCTAssertEqual(newCardParameter.repetition, 1)
+        XCTAssertEqual(newCardParameter.interval, SM2LibTests.defaultInterval)
+        XCTAssertEqual(newCardParameter.easinessFactor, expectedEasinessFactor)
+        XCTAssertEqual(newCardParameter.previousDate, SM2LibTests.october_23_2016)
+        XCTAssertEqual(newCardParameter.nextDate, SM2LibTests.october_23_2016)
     }
 
     func testgrade_4_response() {
@@ -84,14 +84,14 @@ final class SM2LibTests: XCTestCase {
         let oneDay = 86400.0
         let october_24_2016 = SM2LibTests.october_23_2016 + oneDay
         let expectedEasinessFactor = SM2LibTests.defaultEasinessFactor
-        let gradeData = SM2LibTests.defaultGradeData
-        let newGradeData = SM2Engine.gradeFlashCard(gradeData: gradeData, grade: sm2grade, currentDatetime: SM2LibTests.october_23_2016)
+        let cardParameter = SM2LibTests.defaultCardParameter
+        let newCardParameter = SM2Engine.gradeFlashCard(cardParameter: cardParameter, grade: sm2grade, currentDatetime: SM2LibTests.october_23_2016)
 
-        XCTAssertEqual(newGradeData.repetition, 1)
-        XCTAssertEqual(newGradeData.interval, 1)
-        XCTAssertEqual(newGradeData.easinessFactor, expectedEasinessFactor)
-        XCTAssertEqual(newGradeData.previousDate, SM2LibTests.october_23_2016)
-        XCTAssertEqual(newGradeData.nextDate, october_24_2016)
+        XCTAssertEqual(newCardParameter.repetition, 1)
+        XCTAssertEqual(newCardParameter.interval, 1)
+        XCTAssertEqual(newCardParameter.easinessFactor, expectedEasinessFactor)
+        XCTAssertEqual(newCardParameter.previousDate, SM2LibTests.october_23_2016)
+        XCTAssertEqual(newCardParameter.nextDate, october_24_2016)
     }
 
     func testgrade_5_response() {
@@ -99,14 +99,14 @@ final class SM2LibTests: XCTestCase {
         let oneDay = 86400.0
         let october_24_2016 = SM2LibTests.october_23_2016 + oneDay
         let expectedEasinessFactor = 2.6
-        let gradeData = SM2LibTests.defaultGradeData
-        let newGradeData = SM2Engine.gradeFlashCard(gradeData: gradeData, grade: sm2grade, currentDatetime: SM2LibTests.october_23_2016)
+        let cardParameter = SM2LibTests.defaultCardParameter
+        let newCardParameter = SM2Engine.gradeFlashCard(cardParameter: cardParameter, grade: sm2grade, currentDatetime: SM2LibTests.october_23_2016)
 
-        XCTAssertEqual(newGradeData.repetition, 1)
-        XCTAssertEqual(newGradeData.interval, 1)
-        XCTAssertEqual(newGradeData.easinessFactor, expectedEasinessFactor)
-        XCTAssertEqual(newGradeData.previousDate, SM2LibTests.october_23_2016)
-        XCTAssertEqual(newGradeData.nextDate, october_24_2016)
+        XCTAssertEqual(newCardParameter.repetition, 1)
+        XCTAssertEqual(newCardParameter.interval, 1)
+        XCTAssertEqual(newCardParameter.easinessFactor, expectedEasinessFactor)
+        XCTAssertEqual(newCardParameter.previousDate, SM2LibTests.october_23_2016)
+        XCTAssertEqual(newCardParameter.nextDate, october_24_2016)
     }
 
     func testgrade_5_long_response() {
@@ -114,29 +114,29 @@ final class SM2LibTests: XCTestCase {
         let oneDay = 86400.0
         let november_8_2016 = SM2LibTests.october_23_2016 + (oneDay * 16)
         let expectedEasinessFactor = 2.6
-        let gradeData = SM2GradeData(repetition: 6, interval: 6, easinessFactor: 2.5, previousDate: SM2LibTests.october_23_2016, nextDate: SM2LibTests.october_23_2016)
+        let cardParameter = SM2CardParameter(repetition: 6, interval: 6, easinessFactor: 2.5, previousDate: SM2LibTests.october_23_2016, nextDate: SM2LibTests.october_23_2016)
 
-        let newGradeData = SM2Engine.gradeFlashCard(gradeData: gradeData, grade: sm2grade, currentDatetime: SM2LibTests.october_23_2016)
+        let newCardParameter = SM2Engine.gradeFlashCard(cardParameter: cardParameter, grade: sm2grade, currentDatetime: SM2LibTests.october_23_2016)
 
-        XCTAssertEqual(newGradeData.repetition, 7)
-        XCTAssertEqual(newGradeData.interval, 16)
-        XCTAssertEqual(newGradeData.easinessFactor, expectedEasinessFactor)
-        XCTAssertEqual(newGradeData.previousDate, SM2LibTests.october_23_2016)
-        XCTAssertEqual(newGradeData.nextDate, november_8_2016)
+        XCTAssertEqual(newCardParameter.repetition, 7)
+        XCTAssertEqual(newCardParameter.interval, 16)
+        XCTAssertEqual(newCardParameter.easinessFactor, expectedEasinessFactor)
+        XCTAssertEqual(newCardParameter.previousDate, SM2LibTests.october_23_2016)
+        XCTAssertEqual(newCardParameter.nextDate, november_8_2016)
     }
 
     func testgrade_6_response() {
         let sm2grade = SM2Grade.pass
         let expectedEasinessFactor = 1.3
-        let gradeData = SM2GradeData(repetition: 1, interval: 1, easinessFactor: 1.2, previousDate: SM2LibTests.october_23_2016, nextDate: SM2LibTests.october_23_2016)
+        let cardParameter = SM2CardParameter(repetition: 1, interval: 1, easinessFactor: 1.2, previousDate: SM2LibTests.october_23_2016, nextDate: SM2LibTests.october_23_2016)
 
-        let newGradeData = SM2Engine.gradeFlashCard(gradeData: gradeData, grade: sm2grade, currentDatetime: SM2LibTests.october_23_2016)
+        let newCardParameter = SM2Engine.gradeFlashCard(cardParameter: cardParameter, grade: sm2grade, currentDatetime: SM2LibTests.october_23_2016)
 
-        XCTAssertEqual(newGradeData.repetition, 2)
-        XCTAssertEqual(newGradeData.interval, SM2LibTests.defaultInterval)
-        XCTAssertEqual(newGradeData.easinessFactor, expectedEasinessFactor)
-        XCTAssertEqual(newGradeData.previousDate, SM2LibTests.october_23_2016)
-        XCTAssertEqual(newGradeData.nextDate, SM2LibTests.october_23_2016)
+        XCTAssertEqual(newCardParameter.repetition, 2)
+        XCTAssertEqual(newCardParameter.interval, SM2LibTests.defaultInterval)
+        XCTAssertEqual(newCardParameter.easinessFactor, expectedEasinessFactor)
+        XCTAssertEqual(newCardParameter.previousDate, SM2LibTests.october_23_2016)
+        XCTAssertEqual(newCardParameter.nextDate, SM2LibTests.october_23_2016)
     }
 
         static var allTests = [
